aa8b8e0b450830f0363766657ffd3786e3bc2be3
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2024-02-28 19:10:32 +0530

    Added Posts

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -1,19 +1,50 @@
+import React from 'react'
+import styles from './Post.module.css';
 export default function Post() {
   return (
-    <>
-      <div className="card container" style={{ width: "45rem" }}>
-        <img src="https://t4.ftcdn.net/jpg/02/10/96/95/360_F_210969565_cIHkcrIzRpWNZzq8eaQnYotG4pkHh0P9.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </>
-  );
+    
+        <div className={styles.post}>
+          <div className={styles.userpost}>
+          <img className={styles.pstimg}src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Ayodhya_Ram_Mandir_Inauguration_Day_Picture.jpg/1200px-Ayodhya_Ram_Mandir_Inauguration_Day_Picture.jpg' alt='ahh...snap :-('/>
+          <div className={styles.reactions}>
+          <i className='' class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-comments"></i>
+          <i class="fa-regular fa-share-from-square"></i>
+          </div>
+          </div>
+          <div className={styles.userpost}>
+          <img className={styles.pstimg}src='https://t4.ftcdn.net/jpg/04/08/25/05/360_F_408250543_MVaEVGeWxb4FiFy7mEGKj8nfYkwoAZON.jpg' alt='ahh...snap :-('/>
+          <div className={styles.reactions}>
+          <i className='' class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-comments"></i>
+          <i class="fa-regular fa-share-from-square"></i>
+          </div>
+          </div>
+          <div className={styles.userpost}>
+          <img className={styles.pstimg}src='https://fotorbit.com/wp-content/uploads/sites/56/2020/03/varanasi-portrait_tania-chatterjee_3-1000x500.jpg' alt='ahh...snap :-('/>
+          <div className={styles.reactions}>
+          <i className='' class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-comments"></i>
+          <i class="fa-regular fa-share-from-square"></i>
+          </div>
+          </div>
+          <div className={styles.userpost}>
+          <img className={styles.pstimg}src='https://media.licdn.com/dms/image/D4D12AQG56UPUtPJj0w/article-cover_image-shrink_600_2000/0/1669373321238?e=2147483647&v=beta&t=uUMorY6yjVASSX_Iy-VNu8eXkAMfkmY97PWk7zDKR0w' alt='ahh...snap :-('/>
+          <div className={styles.reactions}>
+          <i className='' class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-comments"></i>
+          <i class="fa-regular fa-share-from-square"></i>
+          </div>
+          </div>
+          <div className={styles.userpost}>
+          <img className={styles.pstimg}src='https://media.licdn.com/dms/image/D4D12AQG56UPUtPJj0w/article-cover_image-shrink_600_2000/0/1669373321238?e=2147483647&v=beta&t=uUMorY6yjVASSX_Iy-VNu8eXkAMfkmY97PWk7zDKR0w' alt='ahh...snap :-('/>
+          <div className={styles.reactions}>
+          <i className='' class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-comments"></i>
+          <i class="fa-regular fa-share-from-square"></i>
+          </div>
+          </div>
+          </div>
+    
+  )
 }
--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import styles from './SideBar.module.css';
 export default function SideBar() {
   return (
-    <div className='sideBar'>
-      <ul className="side">
-        <li className="sidelist"></li>
-        <li className="sidelist"></li>
-        <li className="sidelist"></li>
-        <li className="sidelist"></li>
-      </ul>
-    </div>
+    <div className={styles.sidenav}>
+     <ul className={styles.sidemenu}>
+        <li className={styles.courseitems}>HTML</li>
+        <li className={styles.courseitems}>CSS</li>
+        <li className={styles.courseitems}>JavaScript</li>
+        <li className={styles.courseitems}>ReactJS</li>
+        <li className={styles.courseitems}>Redux.js</li>
+        <li className={styles.courseitems}>Java</li>
+        <li className={styles.courseitems}>Data Structures</li>
+        <li className={styles.courseitems}>Algorithms</li>
+        <li className={styles.courseitems}>GitHub</li>
+        <li className={styles.courseitems}>Version Control Systems</li>
+        
+     </ul>
+      </div>
   )
 }
